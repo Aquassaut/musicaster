23a6bc5b67042d133d7285842e05fd80bb9f5c1e
Aquassaut <jeremy.autran@gmail.com>
2014-12-13 21:49:55 +0100

    Big refactor with better dependency model
    Modules don't know about each others and are orchestrated by app.js main
    application file

--- app.js
+++ app.js
@@ -1,94 +1,32 @@
-var http = require('http'),
-    fs = require('fs'),
-    watch = require('node-watch'),
-    path = require('path'),
-    util = require('util'),
-    log = require('npmlog'),
-    url = require('url'),
-    cfg = require('./config.json'),
-    streamDirectoryContent = require('./stream.js'),
-    generateFeed = require('./feed.js');
+var log = require('npmlog'),
+    cfg = require('./lib/config.json'),
+    server = require('./lib/server.js'),
+    watcher = require('./lib/watcher.js'),
+    feed = require('./lib/feed.js');
 
 /**
  *  TODO:
  *  Use more async calls with promises.
- *  Prevent watcher to get crazy when copying a big file over
  */
 
-//Fix music folder setting so we accept string, but work with array
-if (! util.isArray(cfg.music_folder)) {
-    cfg.music_folder = [ cfg.music_folder ];
-}
 var xml = "generating xml...";
+var folders = cfg.music_folder || ".";
+var port = cfg.port || 3333;
+var feedinfo = cfg.feed || {
+    "title": "Feed Title",
+    "description": "A great feed",
+    "feed_url": "127.0.0.1",
+    "site_url": "127.0.0.1"
+};
+var url = feedinfo.feed_url || "127.0.0.1";
 
-/* Server part */
-http.createServer(function (req, res) {
-    res.on('sendStarted', function() {
-        log.info(
-            'server', '[%s] %s %s (%d)',
-            req.connection.remoteAddress, req.method, req.url, res.statusCode
-        );
+var webserver = server.startServer(xml, folders, port, url);
+watcher.startWatching(folders, function() {
+    feed.generate(folders, port, feedinfo, function(err, xml) {
+        if (err)  return log.error(err);
+        webserver.reloadXml(xml);
     });
-    if (req.url === '/') {
-        res.writeHead(200, {'Content-Type': 'text/xml; charset=UTF-8'});
-        res.end(xml.replace(/@@HOST@@/g, (req.headers.host || cfg.site_url)));
-        res.emit('sendStarted');
-    } else {
-        var queried = url.parse(decodeURI(req.url).replace(/\.mp3$/g, '')).path.slice(1);
-        var found = false;
-        cfg.music_folder.forEach(function(folder, idx, ary) {
-            if (found) return;
-            log.info('matcher', 'Trying to find %s in %s', queried, folder);
-            var isLastFolder = (idx === ary.length - 1);
-            var directory = path.resolve(folder, queried);
-            if (fs.existsSync(directory)) {
-                found = true;
-                streamDirectoryContent(directory, res);
-            } else if (isLastFolder) {
-                res.writeHead(404);
-                res.end();
-                res.emit('sendStarted');
-            }
-        });
-    }
-}).listen(cfg.port, function() {
-    log.info('server', 'Server listening on port %d', cfg.port);
 });
 
-/* Directory watcher part */
-var regenerator = (function() {
-    var _generate = function() {
-        if (buildlater) {
-            buildlater = false;
-            build.close();
-            build = setTimeout(_generate, timeout);
-            return;
-        }
-        log.info("feed generation", "generating feed");
-        generateFeed().then(function(output) { xml = output; });
-        buildlater = false;
-        timeout = 5000;
-    }
 
-    var build = setTimeout(_generate, 1);
-    var timeout = 5000;
-    var buildlater = false;
-
-    return function(filename) {
-        if (buildlater) {
-            return;
-        } else if (build._idleNext !== null) {
-            //build already queued
-            log.info('watcher', 'Quick changes detected in filesystem, waiting before taking action');
-            timeout *= 2;
-            buildlater = true;
-        } else {
-            log.info('watcher', '%s changed on disk, queuing RSS regeneration', filename);
-        }
-        build.close();
-        build = setTimeout(_generate, timeout);
-    };
-})();
-
-watch(cfg.music_folder, { followSymLinks:true, maxSymLevel:10 }, regenerator);
 
